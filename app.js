469662471e87e0c971e7ce858406a4484423c650
cesar0987 <cesarbenitez46@gmail.com>
2025-07-31 11:04:17 -0300

    Funciones

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+// Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let amigoValue = document.querySelector("#amigo");
+  if (amigoValue.value === "") {
+    alert("Porfavor agregar un nombre");
+  } else amigos.push(amigoValue.value);
+  console.log("Amigo Agregado");
+  amigoValue.value = "";
+  actualizarLista();
+}
+
+function actualizarLista(params) {
+  let listaSorteo = document.querySelector("#listaAmigosSecretos");
+  listaSorteo.innerHTML = "";
+
+  amigos.forEach((element) => {
+    listaSorteo.innerHTML += `<li>${element}</li>`;
+  });
+}
+function sorteoAmigos(amigosLista) {
+  let listaSorteo = document.querySelector("#listaAmigosSecretos");
+  listaSorteo.innerHTML = "";
+  let seleccionado = Math.floor(Math.random() * amigos.length);
+  console.log(seleccionado);
+  if (amigos.length === 0) {
+    alert("Ya no hay amigos por sortear");
+  } else {
+    listaSorteo.innerHTML += `<li > Tu amigo secreto es ${amigos[seleccionado]}</li>`;
+    amigos.splice(seleccionado, 1);
+  }
+}
